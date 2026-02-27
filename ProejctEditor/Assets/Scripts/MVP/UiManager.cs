7167700af4c945057de83bd24af2402b94672a3b
MinespaceJaeok <MinespaceJaeok@gmail.com>
2026-02-27 18:07:03 +0900

    ㅁㄴㅇ
    ㅁㄴㅇ

--- ProejctEditor/Assets/Scripts/MVP/UiManager.cs
+++ ProejctEditor/Assets/Scripts/MVP/UiManager.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
 using System;
+using TMPro;
 
 
 public class UiManager : MonoBehaviour
 {
-        
+    public TextMeshPro data1;
+    public TextMeshPro data2;
+    public TextMeshPro data3;
+    public TextMeshPro data4;
+    public TextMeshPro data5;
+    public TextMeshPro data6;
+    public TextMeshPro data7;
+
 }
